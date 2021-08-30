64af3dca850ce723bf13fce4e19db30164be8d69
Ondrik, Peter (GED-SDC5) <Peter.Ondrik@BSHG.COM>
2021-08-30 17:57:01 +0200

    Added an example of calling Sy2 Parser functions in Python

---
+++ Sy2Parser.Python.ConsoleApp/Sy2Parser.Python.ConsoleApp.py
@@ -0,0 +1,124 @@
+from ctypes import *
+import os
+from enum import IntEnum, unique
+
+# the dll loader
+# windll libraries call functions using the stdcall calling convention
+# functions are accessed as attributes of the sy2dll object
+sy2dll = windll.LoadLibrary("sy2parser-db.dll")
+
+# Sy2 Parser status codes
+@unique
+class T_Sy2ParserStatusCode(IntEnum):
+	SY2_SUCCESS					= 0x0000			# The operation completed successfully.
+	SY2_FAILD					= 0x0001			# The operation failed.
+	SY2_FILE_NOT_FOUND			= 0x0002			# File not found.
+	SY2_EOF						= 0x0003			# End of file.
+	SY2_INVALID_HANDLE			= 0x0004			# An invalid parser handle.
+	SY2_INPUT_MISMATCHED		= 0x1000			# This signifies any kind of mismatched input error such as when the current input does not match the expected token.
+	SY2_EXTRAENOUS_INPUT		= 0x1001			# A syntax error which requires the removal of a token from the input stream.
+	SY2_NO_VIABLE_ALTERNATIVE	= 0x1002			# Indicates that the parser could not decide which of two or more paths to take based upon the remaining input.
+	SY2_MISSING_TOKEN			= 0x1003			# This method is called to report a syntax error which requires the insertion of a missing token into the input stream.
+	SY2_PARSING_ABORTED			= 0x1FFE			# Occurs when parsing was canceled by user.
+	SY2_UNKNOWN					= 0x1FFF			# An unknown error.
+
+# Sy2 Node types
+@unique
+class T_Sy2NodeType(IntEnum):
+	SY2_UNSPECIFIED		= 0
+	SY2_FILE			= 1
+	SY2_COMMAND			= 2
+	SY2_CMD_VALUE		= 3
+	SY2_TYPEDEF			= 4
+	SY2_SYMBOL			= 5
+	SY2_TYPE			= 6
+	SY2_NAME			= 7
+	SY2_BITMASK			= 8
+	SY2_OFFSET			= 9
+	SY2_ADDRESS			= 10
+	SY2_ENUM_VALUE		= 11
+	SY2_SIGNATURE		= 12
+	SY2_VOID			= 13
+	SY2_BOOL			= 14
+	SY2_INT				= 15
+	SY2_UINT			= 16
+	SY2_FLOAT			= 17
+	SY2_STRUCT			= 18
+	SY2_POINTER			= 19
+	SY2_FUNCTION		= 20
+	SY2_RETURN			= 21
+	SY2_PARAMETER		= 22
+	SY2_TYPE_QUALIFIER	= 23
+	SY2_SIZE			= 24
+	SY2_ARRAY_SIZE		= 25
+	SY2_NODE_COUNT		= 26
+
+# The basic information to unique Sy2 node identification. The Sy2 node represents a terminal and a non-terminal lexical token.
+class T_Sy2Node(Structure):
+	_fields_ = [("type", c_uint),
+				("value", c_char * 512),
+				("depth", c_uint),
+				("line", c_uint),
+				("column", c_uint),
+				("status", c_uint)
+				]
+
+# represents the date and time of a parsed file
+class T_FileDateTime(Structure):
+	_fields_ = [("year", c_ushort),
+				("month", c_ushort),
+				("dayOfWeek", c_ushort),
+				("day", c_ushort),
+				("hour", c_ushort),
+				("minute", c_ushort),
+				("second", c_ushort),
+				("milliseconds", c_ushort)
+				]
+
+# represents a parsed file information
+class T_Sy2FileInfo(Structure):
+    _fields_ = [("size", c_uint),
+                ("creation", T_FileDateTime),
+                ("lastAccess", T_FileDateTime),
+                ("lastWrite", T_FileDateTime)
+                ]
+
+# status code definition
+status = c_uint()
+# file handle definition
+handle = c_uint()
+# info about an open file
+fileInfo = T_Sy2FileInfo()
+# sy2 node
+node = T_Sy2Node()
+
+status = sy2dll.sy2GetApiVersion()
+print("The SY2 API Version: " + hex(status))
+
+status = sy2dll.sy2GetDllVersion()
+print("The SY2 Parser Version: " + hex(status))
+
+# initialization
+sy2file = create_string_buffer(256)
+sy2file.value = os.path.abspath("../../../Test/In/test.sy2").encode()
+status = sy2dll.sy2Open(sy2file, byref(handle))
+if status == T_Sy2ParserStatusCode.SY2_SUCCESS:
+	status = sy2dll.sy2GetFileInfo(handle, byref(fileInfo));
+	print("Size: {} B".format(fileInfo.size))
+	print("Creation: {}-{}-{}".format(fileInfo.creation.year, fileInfo.creation.month, fileInfo.creation.day))
+
+	# parsing
+	status = sy2dll.sy2Parse(handle);
+	if status == T_Sy2ParserStatusCode.SY2_SUCCESS:
+		# printing AST
+		status = sy2dll.sy2ReadNext(handle, byref(node))
+		while status != T_Sy2ParserStatusCode.SY2_EOF:
+			if node.type == T_Sy2NodeType.SY2_COMMAND:
+				print("")
+			print("{}: {}".format(T_Sy2NodeType(node.type).name, node.value))
+			status = sy2dll.sy2ReadNext(handle, byref(node))
+
+	# closing
+	status = sy2dll.sy2Close(handle)
+else:
+	print("sy2Open() failed: ", hex(status))
