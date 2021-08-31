6883eb82b7885c659a47fe2d716a56ca51c7c9c4
Ondrik, Peter (GED-SDC5) <Peter.Ondrik@BSHG.COM>
2021-08-31 18:43:19 +0200

    Updated the Sy2Parser.Python project

--- Sy2Parser.Python/reading_during_parsing.py
+++ Sy2Parser.Python/reading_during_parsing.py
@@ -2,14 +2,13 @@ from sy2parser_wrapper import *
 from os import path
 
 # display of the parsed node
-def showNode(handle, node, callbackContext):
-    # gets pointer (pNode) from address (node)
-	pNode = cast(node, POINTER(T_Sy2Node)).contents
-	print("{}: {}".format(T_Sy2NodeType(pNode.type).name, pNode.value))
+def showNode(handle, addrNode, callbackContext):
+	node = cast(addrNode, POINTER(T_Sy2Node)).contents
+	print("{}: {}".format(T_Sy2NodeType(node.type).name, node.value))
 
 	newNode = T_Sy2Node()
 	status = sy2dll.sy2ReadNext(handle, byref(newNode))
-	while newNode.depth > pNode.depth:
+	while newNode.depth > node.depth:
 		print("{}: {}".format(T_Sy2NodeType(newNode.type).name, newNode.value))
 		status = sy2dll.sy2ReadNext(handle, byref(newNode))
 	print()
@@ -26,7 +25,7 @@ print()
 
 # initialization
 sy2file = create_string_buffer(256)
-sy2file.value = path.abspath("../../../Test/In/test.sy2").encode()
+sy2file.value = path.abspath("../../../Test/In/test-19.sy2").encode()
 status = sy2dll.sy2Open(sy2file, byref(handle))
 if status == T_Sy2ParserStatusCode.SY2_SUCCESS:
 	status = sy2dll.sy2GetFileInfo(handle, byref(fileInfo));
